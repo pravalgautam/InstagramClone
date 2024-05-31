90e6eaa5303e950bbce891901276f2fbe29c4eaa
Praval Gautam <pravalgautam79@gmail.com>
2024-05-31 18:41:07 +0530

    Folder Structure

---
+++ InstagramClone/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Praval Gautam on 31/05/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramClone/Controllers/Others/Onboarding/RegistrationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationViewController.swift
+//  InstagramClone
+//
+//  Created by Praval Gautam on 31/05/24.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
